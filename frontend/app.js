01aa33fe1b6ce6a066b82450e7ec852e099a0ce3
Sercan Çintosun <sercancintosun000@gmail.com>
2026-03-03 17:11:30 +0300

    ana sayfa düzeltildi

--- frontend/app.js
+++ frontend/app.js
@@ -27,85 +27,39 @@ async function getAll(){
         chassisGrid.innerHTML = ""
 
         data.data.forEach(result => {
-            const chassisCard = document.createElement('div')
-            chassisCard.className = "chassis-card"
-
-            const cardBody = document.createElement('div')
-            cardBody.className = "card-body"
-
-            const cardTop = document.createElement("div")
-            cardTop.className = "card-top"
-
-            const code = document.createElement("span")
-            code.textContent = result.code
-            code.className = "code"
             
-
-            const brand = document.createElement('span')
-            brand.className = "brand"
-            brand.textContent = result.brand
-           
-
-            const chassisModel = document.createElement('div')
-            chassisModel.className = "chassis-model"
-            chassisModel.textContent = result.model
-
-            const chassisYear = document.createElement('div')
-            chassisYear.className = "chassis-year"
-            chassisYear.textContent = `${result.production.start} - ${result.production.end}`
-
-
-            const cardTags = document.createElement("div")
-            cardTags.className = "card-tags"
-
-            const tag = document.createElement("span")
-            tag.className = "tag"
-            tag.textContent = result.body_styles[0].type
-
-            const cardEngines = document.createElement('div')
-            cardEngines.className = 'card-engines'
-
-            const engineRow = document.createElement("div")
-            engineRow.className = "engine-row"
+            const cardHTML = `
+            <div class="chassis-card">
+                    <div class="card-body">
+                        <div class="card-top">
+                            <span class="code">${result.code}</span>
+                            <span class="brand">${result.brand}</span>
+                        </div>
+                        <div class="chassis-model">${result.model}</div>
+                        <div class="chassis-year">${result.production.start} - ${result.production.end}</div>
+                        <div class="card-tags">
+                           ${result.body_styles.map(style => `<span class="tag">${style.type} </span>`).join('')}
+                            
+                        </div>
+                        <div class="card-engines">
+                           ${result.engines.map(engine => `
+                                <div class=engine-row>
+                                    <span class="engine-code">${engine.engine_code}</span>
+                                    <span class="engine-power">${engine.power_hp} HP</span>     
+                            </div>
+                            
+                            `).join('')}
+                           
+                        </div>
+                    </div>
+                    <div class="card-image">
+                        <img src="${result.images[0].url}" alt="">
+                    </div>
+                </div>
+                           `
             
-            const engineCode = document.createElement('span')
-            engineCode.className = 'engine-code'
-            engineCode.textContent = result.engines[0].engine_code
-           
-            const enginePower = document.createElement('span')
-            enginePower.className = 'engine-power'
-            enginePower.textContent = result.engines[0].power_hp
-
-            const cardImage = document.createElement('div')
-            cardImage.className = "card-image"
-            
-            const image = document.createElement("img")
-            image.src = result.images[0].url
-           
-
-
-            engineRow.appendChild(enginePower)
-            engineRow.appendChild(engineCode)
-            cardEngines.appendChild(engineRow)
-            cardTags.appendChild(tag)
-            cardTop.appendChild(brand)
-            cardTop.appendChild(code)
-            
-            cardBody.appendChild(cardTop)
-            cardBody.appendChild(chassisModel)
-            cardBody.appendChild(chassisYear)
-            cardBody.appendChild(cardTags)
-            cardBody.appendChild(cardEngines)
-
-            chassisCard.appendChild(cardBody)
-
-            cardImage.appendChild(image)
-            chassisCard.appendChild(cardImage)
-
-            chassisGrid.appendChild(chassisCard)
-
-
-
+                           
+            chassisGrid.insertAdjacentHTML('beforeend', cardHTML);    
         
         })
     }catch(error){
